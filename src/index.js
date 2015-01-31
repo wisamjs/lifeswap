cc908e71f267d7dc6e92be35a25aa8807c4e5049
wzaghal <wisamzaghal@gmail.com>
2015-01-31 14:49:00 -0500

    Add functions to play with state

--- src/index.js
+++ src/index.js
@@ -5,4 +5,48 @@ var HelloWorld = React.createClass({
     }
 });
 
-React.render(<HelloWorld />, document.getElementById('app'));
+
+var HelloUser = React.createClass({
+    getInitialState: function(){
+      return {
+        username: 'Jess!',
+        age:30,
+        pass: 'fakePassword'
+      }
+    },
+
+    handleName: function(e){
+      this.setState({
+        username:e.target.value
+      });
+    },
+    handleAge: function(e){
+      this.setState({
+        age:e.target.value
+      });
+    },
+    handlePass: function(e){
+      this.setState({
+        pass:e.target.value
+      });
+    },
+    reduceAge: function(){
+      this.setState({
+        age:this.state.age -1
+      });
+    },
+    render:function(){
+      return (
+        <div>
+          Hello {this.state.username}, you are {this.state.age} years old! And you probably should not share your password which is {this.state.pass}<br/>
+          change name: <input type="text" value={this.state.username} onChange={this.handleName}/>
+          <br/>Decrease age here: <button onClick={this.reduceAge}>test</button>
+          change pass: <input type="password" value={this.state.pass} onChange={this.handlePass}/>
+
+        </div>
+        )
+    }
+});
+
+// React.render(<HelloWorld />, document.getElementById('app'));
+React.render(<HelloUser />, document.getElementById('app'));
