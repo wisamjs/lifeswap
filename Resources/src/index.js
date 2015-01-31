038b862b40e6900a4bc7c1e5d7f42d03f81fb714
Jessica Duarte <jessicanduarte@gmail.com>
2015-01-31 16:49:08 -0500

    adding html

--- Resources/src/index.js
+++ Resources/src/index.js
@@ -4,8 +4,18 @@ var HelloWorld = React.createClass({
         return <div>Hello World</div>;
     }
 });
+<<<<<<< Updated upstream:Resources/src/index.js
 
 
 
 
 React.render(<HelloWorld />, document.getElementById('app'));
+=======
+ 
+React.render(<HelloWorld />, document.getElementById('app'));
+
+// React.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('app')
+// );
+>>>>>>> Stashed changes:index.js
