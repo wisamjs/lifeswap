fa33ae96411ee9409cd169555105b127b4260ddf
wzaghal <wisamzaghal@gmail.com>
2015-03-07 16:03:49 -0500

    restructure components after merge

--- client/app/jsx/form.js
+++ client/app/jsx/form.js
@@ -1,49 +1,75 @@
-var HelloUser = React.createClass({
+var FirstName = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <label for={this.props.id}>First Name: </label>
+        <input id={this.props.id} placeholder="Enter your first name" type="text" value={this.props.value} onChange={this.props.onChange} />
+      </div>
+      )
+  }
+});
+
+var LastName = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <label for={this.props.id}>Last Name: </label>
+        <input id={this.props.id} placeholder="Enter your last name" type="text" value={this.props.value} onChange={this.props.onChange} />
+      </div>
+      )
+  }
+});
+
+var Location = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <label for={this.props.id}>Location: </label>
+        <input id={this.props.id} placeholder="Enter your location" type="text" value={this.props.value} onChange={this.props.onChange} />
+      </div>
+      )
+  }
+});
+
+var Property = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <label for={this.props.id}>Property Info: </label>
+        <textarea id={this.props.id} placeholder="Enter details about your home" value={this.props.value} onChange={this.props.onChange} />
+      </div>
+      )
+  }
+});
+
+
+var User = React.createClass({
   getInitialState: function(){
     return {
-      firstname: 'First Name', 
-      lastname: 'Last Name',
+      firstName: 'First Name',
+      lastName: 'Last Name',
       location: 'Toronto, Canada',
       property: 'I live in a sweet apartment'
     }
   },
-  handleFirstName: function(e){
-    this.setState({
-      firstname: e.target.value
-      
-    });
-  },
+  handleChange: function(e){
+    var id = e.target.id;
+    var newState = {};
+    newState[id] = e.target.value;
 
-  handleLastName: function(e){
-    this.setState({
-      lastname: e.target.value
-    });
+    this.setState(newState);
   },
-
-  handleLocation: function(e){
-    this.setState({
-      location: e.target.value
-    });
-  },
-
-  handleProperty: function(e){
-     this.setState({
-       property: e.target.value
-     });
-   },
   render: function(){
     return (
       <div>
-        <p>I am <span class="completeMe">{this.state.firstname}</span> <span class="completeMe">{this.state.lastname}</span> and I have a life to swap. My home is in <span class="completeMe">{this.state.location}.</span></p>
-        <p>{this.state.property}</p>
-        <label for="firstname">First Name: </label>
-        <input id="firstname" placeholder="Enter your first name" type="text" value={this.state.firstname} onChange={this.handleFirstName} />
-        <label for="lastName">Last Name: </label>
-        <input id="lastName" placeholder="Enter your last name" type="text" value={this.state.lastname} onChange={this.handleLastName} />
-        <label for="location">Location: </label>
-        <input id="location" placeholder="Enter location" type="text" value={this.state.location} onChange={this.handleLocation} />
-        <label for="property">Property Info: </label>
-        <textarea id="property" placeholder="Enter details about your home"  value={this.state.property} onChange={this.handleProperty}></textarea>
+        <FirstName id={this.state.firstName} value={this.state.firstName} onChange={this.handleChange}/>
+        <br />
+        <LastName id={this.state.lastName} value={this.state.lastName} onChange={this.handleChange}/>
+        <br />
+        <Location id={this.state.location} value={this.state.location} onChange={this.handleChange}/>
+        <br />
+        <Property id={this.state.property} value={this.state.property} onChange={this.handleChange} />
+        <br />
       </div>
     )
   }
@@ -155,7 +181,8 @@ var Form = React.createClass({
       <div>
         <Title name="Life Swap Form"/>
         <Description info="Fill out the form below:"/>
-        <Amenities/>
+        <User />
+        <Amenities />
       </div>
       )
   }
