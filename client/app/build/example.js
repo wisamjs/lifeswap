e938a88447f190de3e9d05cf4c340966797434dc
wzaghal <wisamzaghal@gmail.com>
2015-03-07 16:06:05 -0500

    delete example file

--- client/app/build/example.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-var HelloWorld = React.createClass({displayName: "HelloWorld",
-    render: function() {
-        return React.createElement("div", null, " Hello World ");
-    }
-});
-
-React.render( React.createElement(HelloWorld, null/) , document.getElementById('app'));
--- client/app/jsx/example.js
+++
@@ -1,8 +0,0 @@
-/** @jsx React.DOM */
-var HelloWorld = React.createClass({
-    render: function() {
-        return <div > Hello World < /div>;
-    }
-});
-
-React.render( < HelloWorld / > , document.getElementById('app'));
